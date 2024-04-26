92aaeae3568570cdf2d35bc22e6e9cd22fbb1ac5
630084142 <630084142@qq.com>
2024-04-26 10:40:08 +0200

    1.calculate metrics of gtvt central slices 2.add surface dice 2d (using monai)

--- py_code/research_analysis.py
+++ py_code/research_analysis.py
@@ -1,16 +1,23 @@
 import os
 
+import cv2
+import numpy as np
+import torch
 from added_path_length import APL
 from custom import Debug, Dict
 from custom import Global as g
 from custom import Json, Nii, Value
+from monai.metrics import compute_surface_dice
 from segment_metric import avg_surface_distance_symmetric, dice, hausdorff_distance_95
-from str_lib import Metric, ObsStudyStep, Stat
-from surface_dice import SurfaceDice
+
+# from skimage import morphology
+from str_lib import Metric, ObsStudyStep, Plane, Stat
+
+# from surface_dice import SurfaceDice
 from tqdm import tqdm
 
 
-def cal_obs_study_metrics(obs_study_id: str):
+def cal_obs_study_metrics_3d(obs_study_id: str):
     # baseline_dir = os.path.join(g.TRAIN_RESULTS_DIR, "baseline_obs.study", "baseline")
 
     if obs_study_id.startswith("idl.gtvn_"):
@@ -26,9 +33,9 @@ def cal_obs_study_metrics(obs_study_id: str):
     )
 
     obs_metrics = Dict()
-    obs_metrics_path = os.path.join(obs_study_dir, "obs_study_metrics.json")
+    obs_metrics_path = os.path.join(obs_study_dir, "obs_study_metrics_3d.json")
     for stat in [Stat.AVG, Stat.MEDIAN]:
-        for i in ["correct.vs.origin"]:
+        for i in ["correct.vs.idl"]:
             for metric in [
                 Metric.DSC,
                 Metric.MSD,
@@ -90,13 +97,13 @@ def cal_obs_study_metrics(obs_study_id: str):
             origin_pred = None
 
         volume_pairs = Dict()
-        volume_pairs["correct.vs.origin"] = (final_pred, origin_pred)
-        # volume_pairs["origin.vs.baseline"] = (origin_pred, baseline_pred)
+        volume_pairs["correct.vs.idl"] = (final_pred, origin_pred)
+        # volume_pairs["idl.vs.baseline"] = (origin_pred, baseline_pred)
         # volume_pairs["correct.vs.baseline"] = (final_pred, baseline_pred)
 
-        for cur_key in volume_pairs.keys():
-            reference = volume_pairs[cur_key][0]
-            test = volume_pairs[cur_key][1]
+        for i in volume_pairs.keys():
+            reference = volume_pairs[i][0]
+            test = volume_pairs[i][1]
 
             # dsc/msd/hd95
             if os.path.exists(patient_dir):
@@ -122,9 +129,9 @@ def cal_obs_study_metrics(obs_study_id: str):
                 msd = 0.0
                 hd95 = 0.0
 
-            obs_metrics[patient][cur_key][Metric.DSC] = dsc
-            obs_metrics[patient][cur_key][Metric.MSD] = msd
-            obs_metrics[patient][cur_key][Metric.HD95] = hd95
+            obs_metrics[patient][i][Metric.DSC] = dsc
+            obs_metrics[patient][i][Metric.MSD] = msd
+            obs_metrics[patient][i][Metric.HD95] = hd95
 
             # added path length
             if os.path.exists(patient_dir):
@@ -135,15 +142,30 @@ def cal_obs_study_metrics(obs_study_id: str):
                 apl_pct = 0.0
                 apl_voxel = 0
 
-            obs_metrics[patient][cur_key][Metric.APL_PCT] = apl_pct
-            obs_metrics[patient][cur_key][Metric.APL_VOXEL] = apl_voxel
+            obs_metrics[patient][i][Metric.APL_PCT] = apl_pct
+            obs_metrics[patient][i][Metric.APL_VOXEL] = apl_voxel
 
             # surface dice
             if os.path.exists(patient_dir):
-                sdsc = SurfaceDice(reference_image=reference, other_image=test)
-                obs_metrics[patient][cur_key][Metric.SDSC] = sdsc.get_surface_dice()
+                test = np.expand_dims(test, axis=0)
+                test = np.expand_dims(test, axis=0)
+                if len(test.shape) == 5:
+                    test = np.transpose(test, (0, 1, 3, 4, 2))
+                reference = np.expand_dims(reference, axis=0)
+                reference = np.expand_dims(reference, axis=0)
+                if len(reference.shape) == 5:
+                    reference = np.transpose(reference, (0, 1, 3, 4, 2))
+                sdsc = compute_surface_dice(
+                    y_pred=torch.tensor(test),
+                    y=torch.tensor(reference),
+                    class_thresholds=[1.0],
+                )
+                # tensor to float
+                obs_metrics[patient][i][Metric.SDSC] = sdsc.item()
+                # sdsc = SurfaceDice(reference_image=reference, other_image=test)
+                # obs_metrics[patient][i][Metric.SDSC] = sdsc.get_surface_dice()
             else:
-                obs_metrics[patient][cur_key][Metric.SDSC] = 1.0
+                obs_metrics[patient][i][Metric.SDSC] = 1.0
 
             # record value for avg and median calculation
             for stat in [Stat.AVG, Stat.MEDIAN]:
@@ -155,11 +177,9 @@ def cal_obs_study_metrics(obs_study_id: str):
                     Metric.APL_VOXEL,
                     Metric.SDSC,
                 ]:
-                    obs_metrics[stat][cur_key][metric].append(
-                        obs_metrics[patient][cur_key][metric]
-                    )
+                    obs_metrics[stat][i][metric].append(obs_metrics[patient][i][metric])
 
-    for i in ["correct.vs.origin"]:
+    for i in ["correct.vs.idl"]:
         for metric in [
             Metric.DSC,
             Metric.MSD,
@@ -178,15 +198,191 @@ def cal_obs_study_metrics(obs_study_id: str):
     Json.save(data=obs_metrics, path=obs_metrics_path)
 
 
-obs_study_id_list = [
-    "idl.gtvn_2024.03.11.09.08.22_Jesper_test",
-    "idl.gtvt_2024.03.11.09.08.22_Jesper_test",
-    "idl.gtvn_2024.03.18.09.05.54_Jesper_research",
-    "idl.gtvt_2024.03.18.09.05.54_Jesper_research",
-    # "idl.gtvn_2024.03.21.13.07.10_Hanna_test",
-    # "idl.gtvt_2024.03.21.13.07.10_Hanna_test",
-    # "idl.gtvn_2024.03.22.10.08.33_Kenneth_test",
-    # "idl.gtvt_2024.03.22.10.08.33_Kenneth_test",
-]
-for obs_study_id in obs_study_id_list:
-    cal_obs_study_metrics(obs_study_id)
+def cal_obs_study_metrics_gtvt_central_slices(obs_study_id: str):
+    if not obs_study_id.startswith("idl.gtvt_"):
+        Debug.error_exit("Must be an 'idl.gtvt' id")
+
+    obs_study_dir = os.path.join(
+        g.TRAIN_RESULTS_DIR, "baseline_obs.study", obs_study_id
+    )
+
+    obs_metrics = Dict()
+    obs_metrics_path = os.path.join(
+        obs_study_dir, "obs_study_metrics_gtvt_central_slices.json"
+    )
+    for stat in [Stat.AVG, Stat.MEDIAN]:
+        for metric in [
+            Metric.DSC,
+            Metric.MSD,
+            Metric.HD95,
+            Metric.APL_PCT,
+            Metric.APL_VOXEL,
+            Metric.SDSC,
+        ]:
+            for plane in [Plane.TRANSVERSE, Plane.CORONAL, Plane.SAGITTAL]:
+                obs_metrics[stat][metric][plane] = []
+    Json.save(data=obs_metrics, path=obs_metrics_path)
+
+    patient_list = []
+    # open "obs_study_step.json" and find approved patients
+    obs_study_step = Json.load(
+        os.path.join(
+            obs_study_dir,
+            "obs_study_step.json",
+        )
+    )
+    for patient in obs_study_step.keys():
+        if obs_study_step[patient] == ObsStudyStep.APPROVED:
+            patient_list.append(patient)
+
+    # loop through patients
+    for patient in tqdm(patient_list):
+        patient_dir = os.path.join(obs_study_dir, "patients", patient)
+        idl_img_dir = os.path.join(patient_dir, "round=01")
+
+        delineation = Nii.load(
+            os.path.join(idl_img_dir, "gtvt_delineation.nii.gz"),
+            binary=True,
+        )
+        origin_pred = Nii.load(
+            os.path.join(idl_img_dir, "gtvt_pred.nii.gz"),
+            binary=True,
+        )
+        correction_mask = Nii.load(
+            os.path.join(idl_img_dir, "gtvt_correction_mask.nii.gz"),
+            binary=True,
+        )
+        correction = Nii.load(
+            os.path.join(idl_img_dir, "gtvt_correction.nii.gz"),
+            binary=True,
+        )
+        final_pred = g.combine_pred_correction(
+            origin_pred=origin_pred,
+            correction=correction,
+            correction_mask=correction_mask,
+        )
+
+        selected_slices = Json.load(os.path.join(patient_dir, "selected_slices.json"))
+
+        for plane in [Plane.TRANSVERSE, Plane.CORONAL, Plane.SAGITTAL]:
+            slice_id = int(selected_slices[plane]["round=01"])
+
+            if plane == Plane.TRANSVERSE:
+                delineation_2d = delineation[slice_id, :, :]
+                final_pred_2d = final_pred[slice_id, :, :]
+            elif plane == Plane.CORONAL:
+                delineation_2d = delineation[:, slice_id, :]
+                final_pred_2d = final_pred[:, slice_id, :]
+            elif plane == Plane.SAGITTAL:
+                delineation_2d = delineation[:, :, slice_id]
+                final_pred_2d = final_pred[:, :, slice_id]
+
+            # Nii.save(delineation_2d, os.path.join(g.DEBUG_DIR, "before_open.nii.gz"))
+            kernel = np.ones((3, 3), np.uint8)
+            delineation_2d = cv2.morphologyEx(delineation_2d, cv2.MORPH_OPEN, kernel)
+            # Nii.save(delineation_2d, os.path.join(g.DEBUG_DIR, "after_open.nii.gz"))
+
+            # dsc/msd/hd95
+            dsc = dice(
+                test=delineation_2d,
+                reference=final_pred_2d,
+                nan_for_nonexisting=False,
+            )
+            msd = avg_surface_distance_symmetric(
+                test=delineation_2d,
+                reference=final_pred_2d,
+                none_for_nonexisting=True,
+                voxel_spacing=(1, 1),  # g.NII_SPACING,
+            )
+            hd95 = hausdorff_distance_95(
+                test=delineation_2d,
+                reference=final_pred_2d,
+                none_for_nonexisting=True,
+                voxel_spacing=(1, 1),  # g.NII_SPACING,
+            )
+            obs_metrics[patient][Metric.DSC][plane] = dsc
+            obs_metrics[patient][Metric.MSD][plane] = msd
+            obs_metrics[patient][Metric.HD95][plane] = hd95
+
+            # added path length
+            apl = APL(reference_structure=final_pred_2d, other_structure=delineation_2d)
+            apl_pct = apl.get_apl(normalized=True)
+            apl_voxel = apl.get_apl(normalized=False)
+            obs_metrics[patient][Metric.APL_PCT][plane] = apl_pct
+            obs_metrics[patient][Metric.APL_VOXEL][plane] = apl_voxel
+
+            # surface dice
+            final_pred_2d = np.expand_dims(final_pred_2d, axis=0)
+            final_pred_2d = np.expand_dims(final_pred_2d, axis=0)
+            if len(final_pred_2d.shape) == 5:
+                final_pred_2d = np.transpose(final_pred_2d, (0, 1, 3, 4, 2))
+            delineation_2d = np.expand_dims(delineation_2d, axis=0)
+            delineation_2d = np.expand_dims(delineation_2d, axis=0)
+            if len(delineation_2d.shape) == 5:
+                delineation_2d = np.transpose(delineation_2d, (0, 1, 3, 4, 2))
+            sdsc = compute_surface_dice(
+                y_pred=torch.tensor(final_pred_2d),
+                y=torch.tensor(delineation_2d),
+                class_thresholds=[1.0],
+            )
+            # tensor to float
+            obs_metrics[patient][Metric.SDSC][plane] = sdsc.item()
+            # sdsc = SurfaceDice(
+            #     reference_image=final_pred_2d, other_image=delineation_2d
+            # )
+            # obs_metrics[patient][Metric.SDSC][plane] = sdsc.get_surface_dice()
+
+            # record value for avg and median calculation
+            for stat in [Stat.AVG, Stat.MEDIAN]:
+                for metric in [
+                    Metric.DSC,
+                    Metric.MSD,
+                    Metric.HD95,
+                    Metric.APL_PCT,
+                    Metric.APL_VOXEL,
+                    Metric.SDSC,
+                ]:
+                    obs_metrics[stat][metric][plane].append(
+                        obs_metrics[patient][metric][plane]
+                    )
+
+    # calculate avg and median
+    for metric in [
+        Metric.DSC,
+        Metric.MSD,
+        Metric.HD95,
+        Metric.APL_PCT,
+        Metric.APL_VOXEL,
+        Metric.SDSC,
+    ]:
+        for plane in [Plane.TRANSVERSE, Plane.CORONAL, Plane.SAGITTAL]:
+            obs_metrics[Stat.MEDIAN][metric][plane] = Value.median(
+                obs_metrics[Stat.MEDIAN][metric][plane]
+            )
+            obs_metrics[Stat.AVG][metric][plane] = Value.avg(
+                obs_metrics[Stat.AVG][metric][plane]
+            )
+
+    Json.save(data=obs_metrics, path=obs_metrics_path)
+
+
+if 1:
+    obs_study_id_list = [
+        "idl.gtvt_2024.03.18.09.05.54_Jesper_research",
+        "idl.gtvt_2024.04.12.12.05.44_Kenneth_research",
+        "idl.gtvt_2024.04.18.11.04.48_Hanna_research",
+        "idl.gtvn_2024.03.18.09.05.54_Jesper_research",
+        "idl.gtvn_2024.04.12.12.05.44_Kenneth_research",
+        "idl.gtvn_2024.04.18.11.04.48_Hanna_research",
+    ]
+    for obs_study_id in obs_study_id_list:
+        cal_obs_study_metrics_3d(obs_study_id)
+
+else:
+    obs_study_id_list = [
+        "idl.gtvt_2024.03.18.09.05.54_Jesper_research",
+        "idl.gtvt_2024.04.12.12.05.44_Kenneth_research",
+        "idl.gtvt_2024.04.18.11.04.48_Hanna_research",
+    ]
+    for obs_study_id in obs_study_id_list:
+        cal_obs_study_metrics_gtvt_central_slices(obs_study_id)
